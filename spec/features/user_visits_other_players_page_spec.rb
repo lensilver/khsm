120b9fc328fcfa229f90ecdbcbea6d4d30efedcb
lensilver <lensilverpr@hotmail.com>
2022-02-10 14:48:57 +0000

    Fixed capybara test

--- spec/features/user_visits_other_players_page_spec.rb
+++ spec/features/user_visits_other_players_page_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.feature 'USER sees profile the other player', type: :feature do
-  let(:user1) { create(:user, name: 'John Doe', id: 1)}
-  let(:user2) { create(:user, name: 'Jane Doe', id: 2)}
+  let(:user1) { create(:user, name: 'John Doe', balance: 1000)}
+  let(:user2) { create(:user, name: 'Jane Doe', balance: 1000)}
 
   let!(:games) do
   	[create(:game, user: user2, created_at: Time.zone.parse('2021.02.07, 13:00'), current_level: 14, prize: 500000),
@@ -10,20 +10,20 @@ RSpec.feature 'USER sees profile the other player', type: :feature do
   end
 
   before do
-	login_as user1
+	  login_as user1
   end
 
-  scenario 'Jone Doe sees profile of Jane Doe' do
+  scenario 'a registered user is viewing another user`s page' do
 
     visit "/"
-    click_link 'Jane Doe'
-
+    click_link 'Jane Doe'    
+    
     expect(page).to have_content '07 февр., 13:00'
     expect(page).to have_content '07 февр., 14:00'
     expect(page).to have_content 'John Doe'
     expect(page).to have_content 'Jane Doe'
-    expect(page).to have_content '14'
     expect(page).to have_content '500 000 ₽'
+    expect(page).to have_content '1 000 ₽'
     expect(page).to have_content 'в процессе'
     expect(page).to have_content 'деньги'
     expect(page).not_to have_content 'Сменить имя и пароль'
